feb15362e38ff6f98247545c01873e76811940fd
제리 <juwonlee920@gmail.com>
2022-10-16 19:11:50 +0900

    Momentum Project # Part 5.3 Recap

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,12 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello(){
-    console.log("hello");
+function getClock(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = (`${hours}:${minutes}:${seconds}`);
 }
 
-setInterval(sayHello, 5000);
+getClock();                                     // 즉시 getClock() 함수를 호출 후
+setInterval(getClock, 1000);                    // 매 초마다 getClock() 실행
